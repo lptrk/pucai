d845ccd74cf3e465b7041dd5a7e8b3e00eb0578c
lptrk <lktpriv@gmail.com>
2024-05-15 19:25:34 +0200

    feat(api/pucai📋): implement PucaiServiceImpl.java

---
+++ src/main/java/dev/lptrk/pucai/service/impl/PucaiServiceImpl.java
@@ -0,0 +1,49 @@
+package dev.lptrk.pucai.service.impl;
+
+import dev.lptrk.pucai.dao.PucaiRepository;
+import dev.lptrk.pucai.model.PucaiEty;
+import dev.lptrk.pucai.service.PucaiService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PucaiServiceImpl implements PucaiService {
+
+    private final PucaiRepository pucaiRepository;
+
+    @Override
+    public List<PucaiEty> findAll() {
+        return this.pucaiRepository.findAll();
+    }
+
+    @Override
+    public List<PucaiEty> findAllByDateAfter(Date date) {
+        return this.pucaiRepository.findAllByDateAfter(date);
+    }
+
+    @Override
+    public PucaiEty findById(Long id) {
+        return this.pucaiRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PucaiEty save(PucaiEty pucaiEty) {
+        return this.pucaiRepository.save(pucaiEty);
+    }
+
+    @Override
+    public PucaiEty updateById(Long id, PucaiEty pucaiEty) {
+        var pucaiToUpdate = this.pucaiRepository.findById(id).orElse(null);
+        pucaiToUpdate = pucaiEty;
+        return this.pucaiRepository.save(pucaiToUpdate);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.pucaiRepository.deleteById(id);
+    }
+}
