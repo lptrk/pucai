0df96776ce5285da4ddc4913f1070ecaeae0aeeb
lptrk <lktpriv@gmail.com>
2024-05-19 13:21:32 +0200

    feat(frontend): implement default layout and navbar view

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {BottomNavigationComponent} from "./components/bottom-navigation/bottom-navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BottomNavigationComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
